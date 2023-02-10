2287abc1af21a9b604c66ade948830d758e2411d
Julien POIRIER <connexion@julienpoirier-webdev.com>
2023-02-10 14:08:18 +0100

    add normalize

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 
+
 function App() {
 
   return (
     <div className="App">
-
+    <h1>Hello world</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A corporis debitis deserunt dolorem, ea eligendi enim expedita facere iure laborum natus, nisi pariatur perferendis quasi, quo reprehenderit saepe sint suscipit.</p>
     </div>
   )
 }
