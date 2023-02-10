3b2026e90e4e25e4798ed86066369909e4092b68
Julien POIRIER <connexion@julienpoirier-webdev.com>
2023-02-10 14:18:17 +0100

    create layout of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css'
+import 'normalize.css'
 
 
 function App() {
 
   return (
     <div className="App">
-    <h1>Hello world</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A corporis debitis deserunt dolorem, ea eligendi enim expedita facere iure laborum natus, nisi pariatur perferendis quasi, quo reprehenderit saepe sint suscipit.</p>
     </div>
   )
 }
