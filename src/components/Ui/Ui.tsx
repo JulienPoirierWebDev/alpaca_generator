f1d2b1f2a2b66190dca290b800728205cbef4eb8
Julien POIRIER <connexion@julienpoirier-webdev.com>
2023-02-13 09:10:10 +0100

    create UI & button component

--- src/components/Ui/Ui.tsx
+++ src/components/Ui/Ui.tsx
@@ -6,6 +6,39 @@ const Ui = () => {
     // TODO
     // create reducer ?? context for state management
 
+    const [actualAccessory, setActualAccessory] = React.useState("hair");
+
+    type Alpaca = {
+        hair: string,
+        ears: string,
+        eyes: string,
+        mouth: string,
+        neck: string,
+        leg: string,
+        accessory: string,
+        background: string
+    }
+
+    type AlpacaElements = "hair" | "ears" | "eyes" | "mouth" | "neck" | "leg" | "accessory" | "background";
+
+    const [alpaca, setAlpaca] = React.useState({
+        hair: "default",
+        ears: "default",
+        eyes: "default",
+        mouth: "default",
+        neck: "default",
+        leg: "default",
+        accessory: "earrings",
+        background: "blue50"
+    });
+
+    const changeAlpaca = (alpaca:Alpaca, changedKey:AlpacaElements, changedValue:string) => {
+        let newAlpaca:Alpaca = {...alpaca};
+        newAlpaca[changedKey] = changedValue;
+
+        setAlpaca(newAlpaca);
+    }
+
     type Styles = {
         [key: string]: string[]
     }
@@ -22,7 +55,6 @@ const Ui = () => {
         background: ["blue50", "blue60", "blue70", "darkblue30", "darkblue50", "darkblue70", "green50", "green60", "green70", "grey40", "grey70", "grey80", "red50", "red60", "red70", "yellow50", "yellow60", "yellow70"]
     };
 
-    const [actualAccessory, setActualAccessory] = React.useState("hair");
 
 
     return (
@@ -38,8 +70,15 @@ const Ui = () => {
                 <h2>Style</h2>
                 {styles[actualAccessory].map((style:string) => {
                     // @ts-ignore
-                    return <Button key={style} element={style} handleClick={function () {console.log("clic")}}/>
+                    return <Button key={style} element={style} accessory={actualAccessory} handleClick={ () => {changeAlpaca(alpaca,actualAccessory,style)}}/>
                 } )}
+            </div>
+            <div>
+                {actualAccessory}
+                {Object.keys(alpaca).map((alpacaKey:string) => {
+                    // @ts-ignore
+                    return <div>{alpaca[alpacaKey]}</div>
+                })}
             </div>
         </div>
     );
