ec38b6e5124ee720837f88febab4614260d8f1f2
Julien POIRIER <connexion@julienpoirier-webdev.com>
2023-02-14 17:59:40 +0100

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import './App.css'
 import 'normalize.css'
 import Title from "./components/Title/Title";
 import Avatar from "./components/Avatar/Avatar";
-import Ui from "./components/Ui/Ui";
 import AlpacaProvider from './context/AlpacaProvider';
+import Ui from './components/Ui/Ui';
 
 
 function App() {
