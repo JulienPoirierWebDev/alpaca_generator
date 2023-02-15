90469899774620851e0de3d289f0932b8b6fd049
Gaëtan H <steellgold0@gmail.com>
2023-02-15 17:02:07 +0100

    Add transparent background

--- src/context/AlpacaProvider.tsx
+++ src/context/AlpacaProvider.tsx
@@ -70,7 +70,7 @@ const AlpacaProvider = ({children}:Props) => {
         neck: ["default", "bend-backward", "bend-forward"],
         leg: ["default", "bubble-tea", "cookie", "game-console", "tilt-backward", "tilt-forward"],
         accessories: ["earings", "flower", "glasses", "headphone"],
-        background: ["blue50", "blue60", "blue70", "darkblue30", "darkblue50", "darkblue70", "green50", "green60", "green70", "grey40", "grey70", "grey80", "red50", "red60", "red70", "yellow50", "yellow60", "yellow70"]
+        background: ["transparent", "blue50", "blue60", "blue70", "darkblue30", "darkblue50", "darkblue70", "green50", "green60", "green70", "grey40", "grey70", "grey80", "red50", "red60", "red70", "yellow50", "yellow60", "yellow70"]
     };
 
     const values = { actualAccessory, setActualAccessory, alpaca, setAlpaca, changeAlpaca, alpacaElements, styles };
